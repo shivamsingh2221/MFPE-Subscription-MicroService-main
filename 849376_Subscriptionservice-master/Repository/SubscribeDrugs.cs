6adc6df72cf1c3ddce13ff86a55edce5be870eab
Hercules <shivamsingh2221@gmail.com>
2020-11-10 19:26:40 +0530

    first commit

---
+++ 849376_Subscriptionservice-master/Repository/SubscribeDrugs.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SubscriptionService.Models;
+using SubscriptionService.Controllers;
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.Extensions.Configuration;
+
+namespace SubscriptionService.Repository
+{
+    public class SubscribeDrugs : ISubscribeDrugs
+    {
+        static List<SubscriptionDetails> details = new List<SubscriptionDetails>() {
+                new SubscriptionDetails{ Id=1, MemberId=1, MemberLocation="Delhi", PrescriptionId=1, RefillOccurrence="weekly", Status=true, SubscriptionDate= Convert.ToDateTime("2020-12-01 01:01:00 AM")},
+                new SubscriptionDetails{ Id=2, MemberId=2, MemberLocation="Kolkata", PrescriptionId=2, RefillOccurrence="Monthly", Status=true, SubscriptionDate= Convert.ToDateTime("2020-12-01 01:01:00 AM")},
+                 new SubscriptionDetails{ Id=3, MemberId=1, MemberLocation="Kolkata", PrescriptionId=3, RefillOccurrence="Monthly", Status=true, SubscriptionDate= Convert.ToDateTime("2020-12-01 01:01:00 AM")},
+                 new SubscriptionDetails{ Id=4, MemberId=1, MemberLocation="Haldwani", PrescriptionId=3, RefillOccurrence="Weekly", Status=true, SubscriptionDate= Convert.ToDateTime("2020-12-01 01:01:00 AM")}
+             };
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(SubscribeController));
+        
+        public SubscriptionDetails PostSubscription(PrescriptionDetails prescription, string PolicyDetails, int Member_Id, string auth)
+        {
+            _log4net.Info("DruApi is being called to check for the availability of the DrugName= "+prescription.DrugName);
+            
+            List<LocationWiseDrug> location = new List<LocationWiseDrug>();
+              var drugs = "";
+              var query = prescription.DrugName;
+              
+              string[] token=auth.Split(" ");
+              
+              HttpClient client = new HttpClient();
+              HttpResponseMessage result=null;
+              
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://52.186.44.253/api/DrugsApi/searchDrugsByName/" + query);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token[1]);
+
+                result = client.SendAsync(request).Result;
+
+                if (!result.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                _log4net.Error("Exception occured in calling Drug Api" + nameof(SubscribeDrugs) + " and error is" + ex.Message);
+
+                return null;
+            }
+            drugs = result.Content.ReadAsStringAsync().Result;
+            location = JsonConvert.DeserializeObject<List<LocationWiseDrug>>(drugs);
+            if (location.Count!=0)
+            {
+                _log4net.Info(prescription.DrugName+" Drug Available");
+                var last = details.Last();
+                SubscriptionDetails subscription= new SubscriptionDetails { Id = (details[details.Count-1].Id+1), MemberId = Member_Id, MemberLocation = "Delhi", PrescriptionId = 3, RefillOccurrence = prescription.RefillOccurrence, Status = true, SubscriptionDate = DateTime.Now };
+                details.Add(subscription);
+                return subscription;
+            }
+            else
+            {
+                _log4net.Info(prescription.DrugName+" Drug NotAvailable");
+                return new SubscriptionDetails { Id = 0, MemberId = 0, MemberLocation = "", PrescriptionId = 0, RefillOccurrence = "", Status = false, SubscriptionDate = Convert.ToDateTime("2020-12-01 01:01:00 AM") };
+            }
+        }
+        public SubscriptionDetails PostUnSubscription(int Member_Id, int Subscription_Id, string auth)
+        {
+
+            // Get the data from refill microservice 
+            _log4net.Info("Checking for Subscriptionid= "+Subscription_Id);
+            
+                SubscriptionDetails result = new SubscriptionDetails();
+                var subs = details.Find(p => p.Id == Subscription_Id);
+                if (subs != null)
+                {
+                    _log4net.Info("Interacting with refill microservice for the payment status for subscription id =" + Subscription_Id);
+
+                try
+                {
+
+                    using (var httpClient = new HttpClient())
+                    {
+                        string[] token=auth.Split(" ");
+                        
+                        var request = new HttpRequestMessage(HttpMethod.Get, "http://52.150.54.3/api/RefillOrders/RefillDues/" + Subscription_Id);
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token[1]);
+
+                        using (var response = httpClient.SendAsync(request).Result)
+                        {
+
+                            if (!response.IsSuccessStatusCode)
+                            {
+                                return result;
+                            }
+
+                            var data = response.Content.ReadAsStringAsync().Result;
+
+                            var due = JsonConvert.DeserializeObject<int>(data);
+
+                            result = subs;
+
+                            _log4net.Info("Number of Refills pending for payment" + due + "for subscriptionId" + Subscription_Id);
+
+
+                            if (due == 0 || due==-1)
+                            {
+                                var unsubscribe = details.Find(p => p.Id == Subscription_Id);
+                                result.Status = false;
+                                details.Remove(unsubscribe);
+                            }
+
+
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    _log4net.Error(ex.Message);
+                    return null;
+                }
+                    return result;
+
+                }
+                return null;
+            
+        }
+    }
+}
---
+++ SubscriptionTest/RepositoryTest.cs
@@ -0,0 +1,83 @@
+﻿using Castle.Components.DictionaryAdapter;
+using Microsoft.AspNet.SignalR.Client.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Moq;
+using NUnit.Framework;
+using SubscriptionService.Controllers;
+using SubscriptionService.Models;
+using SubscriptionService.Provider;
+using SubscriptionService.Repository;
+namespace SubscriptionTest
+{
+    class RepositoryTest
+    {
+        
+        private SubscribeDrugs sub;
+
+       PrescriptionDetails pre = new PrescriptionDetails()
+        {
+            DoctorName = "abc",
+            DrugName = "Disprin",
+            Id = 1,
+            InsurancePolicyNumber = 2,
+            InsuranceProvider = "xyz",
+            PrescriptionDate = DateAndTime.Now,
+            RefillOccurrence = "Weekly"
+
+        };
+        string token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlNhdXJhYmgiLCJlbWFpbCI6InNtMTIzQGd" +
+            "tYWlsLmNvbSIsIm5iZiI6MTYwNDkyODQ0NywiZXhwIjoxNjA0OTI5MzQ2LCJpYXQiOjE2MDQ5Mjg0NDcsImlzcyI6IkludmVudG9yeUF1dGh" +
+            "lbnRpY2F0aW9uU2VydmVyIiwiYXVkIjoiSW52ZW50b3J5U2VydmljZVBvc3RtYW5DbGllbnQifQ.7pw9wLYUC5JcGIuqVHEcDwIy" +
+            "Bpcw6Zb50tVOVvSfrTk";
+        PrescriptionDetails pre1 = new PrescriptionDetails()
+        {
+            DoctorName = "abc",
+            DrugName = "vetmal",
+            Id = 1,
+            InsurancePolicyNumber = 2,
+            InsuranceProvider = "xyz",
+            PrescriptionDate = DateAndTime.Now,
+            RefillOccurrence = "Weekly"
+
+        };
+        [SetUp]
+        public void Setup()
+        {
+            
+            sub = new SubscribeDrugs();
+
+        }
+        [Test]
+        public void PostSubscribe_WhenCalled_returnobject()
+        {
+
+            var res = sub.PostSubscription(pre, "hello", 1,token);
+            Assert.That(res.Status, Is.True);
+
+        }
+        [Test]
+        public void PostSubscribe_DrugNotAvailable_returnnullObject()
+        {
+            var res = sub.PostSubscription(pre1, "hello123", 2,token);
+            Assert.That(res.Status, Is.False);
+        }
+        
+        [Test]
+        public void PostUnSubscribe_PaymentDone_returnObject()
+        {
+           
+            var result = sub.PostUnSubscription(1, 3,token);
+            Assert.That(result.Status, Is.False) ;
+        }
+        [Test]
+        public void PostUnsubscribe_PaymentDue_returnNull()
+        {
+           
+            var result = sub.PostUnSubscription(1, 1,token);
+            Assert.That(result.Status, Is.True);
+        }
+
+        
+    }
+}
